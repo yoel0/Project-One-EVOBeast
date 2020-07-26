f871b0b49f1528baab7705560941648691c3fb4f
yoel <yoel@macbook-pro.lan>
2020-07-25 18:41:14 -0700

    [EVOBeast] Add Draw and Animation Functions

--- script.js
+++ script.js
@@ -6,16 +6,33 @@ canvas.width = window.innerWidth;
 // load images
 const images = {};
 images.player = new Image();
-images.player.src = "character.png";
+images.player.src = "characters/character.png";
 
 const playerWidth = "333.333333";
 const playerHeight = "204.8";
-let playerFrameX = 2;
-let playerFrameY = 2;
+let playerFrameX = 1;
+let playerFrameY = 1;
 let playerX = 0;
 let playerY = 0;
 const playerSpeed = 6;
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-  ctx.drawImage;
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
+
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawSprite(
+    images.player,
+    playerWidth * playerFrameX,
+    playerHeight * playerFrameY,
+    playerWidth,
+    playerHeight,
+    playerX,
+    playerY,
+    playerWidth,
+    playerHeight
+  );
+}
+
+window.onload = setInterval(animate, 1000 / 30);

da55a087b80d04f1b57585b9d3da609656824f83
yoel <yoel@macbook-pro.home>
2020-07-28 15:55:12 -0700

    [EVOBeast] Add Heroine, Rabbit, Hunter

--- script.js
+++ script.js
@@ -1,56 +1,99 @@
-// Get canvas element and declare it in const variable called canvas.
-const canvas = document.getElementById("canvas");
-// ctx shortcut for context this will give access to built in draw in api
-// and now I can call in the ctx variable which holds many built in canvas methods used for drawing text, shapes, images and other objects.
-const ctx = canvas.getContext("2d");
-// I set our canvas to the inner height and width of the browser window.
-// at this point we are saying we are going to define the whole window as our draw space because tiny windows kind of suck.
-canvas.height = window.innerHeight;
-canvas.width = window.innerWidth;
-
-// load images
-// an empty array to hold our images.
-const images = {};
-// this will add player images in to our images array.
-images.player = new Image();
-// setting the images source to pull from our sprite sheet.
-images.player.src = "images/character.png";
-
-// declare a couple variables that will help us navigate and find the characters within the sprite sheet.
-// get the width of one frame in our sprite sheet.
-const playerWidth = "333.333333";
-// get height of one frame in our sprite sheet.
-const playerHeight = "204.8";
-// we will go frame to frame by using X, Y coordinates.
-let playerFrameX = 1;
-let playerFrameY = 1;
-// these will determine at what point we draw our characters on the canvas.
-let playerX = 0;
-let playerY = 0;
-// we will also declare our player speed for now it will be 6 (come back later to change).
-const playerSpeed = 6;
-// now we want to take our sprite sheet choose coordinates of a frame and draw that frame on canvas. To make it RE-useable for other sprite sheets in the game and to customize if needed, I will wrap it in a function called drawSprite.
-// this functions will need 9 attributes, the first one is the image we want to draw in this case our sprite sheet, the next 4 attributes specify area from the src image we want to draw width and height the next 4 attributes will specify where on the canvas we want to draw "Destination" Y and X show where Height and Width define sizing.
-function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
-  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+let movementDisplay;
+let ctx;
+let game;
+let heroine;
+let rabbit;
+let hunter;
+// Crawler Constructor function
+function Crawler(x, y, width, height, color) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.alive = true;
+  this.render = function () {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  };
 }
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSprite(
-    images.player,
-    playerWidth * playerFrameX,
-    playerHeight * playerFrameY,
-    playerWidth,
-    playerHeight,
-    playerX,
-    playerY,
-    playerWidth,
-    playerHeight
-  );
-}
-
-window.onload = setInterval(animate, 1000 / 30);
+const detectHit = () => {
+  // check for collision on x axis
+  // if the hero's bottom value is > rabbit's top value
+  if (
+    hero.x + hero.width > rabbit.x &&
+    hero.x < rabbit.x + rabbit.width &&
+    hero.y + hero.height > rabbit.y &&
+    hero.y < rabbit.y + rabbit.height
+  ) {
+    rabbit.alive = false;
+  }
+  if (
+    hero.x + hero.width > hunter.x &&
+    hero.x < hunter.x + hunter.width &&
+    hero.y + hero.height > hunter.y &&
+    hero.y < hunter.y + hunter.height
+  ) {
+    hunter.alive = false;
+  }
+};
+const gameLoop = () => {
+  // console.log('looping in 💩')
+  // clear the cavas
+  ctx.clearRect(0, 0, game.width, game.height);
+  // display the x, y coordinates of our hero onto the DOM
+  movementDisplay.textContent = `X:${hero.x}\nY:${hero.y}`;
+  // check if the rabbit is alive and
+  // render the hero
+  hero.render();
+  if (rabbit.alive) {
+    // render the rabbit
+    rabbit.render();
+    // check for collision
+    detectHit();
+  }
+  if (hunter.alive) {
+    // render the hunter
+    hunter.render();
+    // checck for collision
+    detectHit();
+  }
+};
+const movementHandler = (e) => {
+  // w: 87, a:65, s:83, d:68
+  switch (e.keyCode) {
+    case 87: // w up
+      if (hero.y > 0) hero.y -= 20;
+      break;
+    case 83: // s down
+      if (hero.y + hero.height < game.height) hero.y += 20;
+      break;
+    case 65: // a left
+      if (hero.x > 0) hero.x -= 20;
+      break;
+    case 68: // d right
+      if (hero.x + hero.width < game.width) hero.x += 20;
+      break;
+    default:
+      console.log("invalid keystroke");
+  }
+};
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM loaded");
+  // DOM REFS
+  movementDisplay = document.getElementById("movement");
+  game = document.getElementById("game");
+  // CANVAS CONFIG
+  game.setAttribute("height", 500);
+  game.setAttribute("width", 800);
+  ctx = game.getContext("2d");
+  // CHARACTER REFS
+  hero = new Crawler(200, 100, 50, 50, "red");
+  rabbit = new Crawler(300, 100, 60, 60, "white");
+  hunter = new Crawler(480, 280, 70, 70, "black");
+  document.addEventListener("keydown", movementHandler);
+  let runGame = setInterval(gameLoop, 60);
+});
 
 window.addEventListener("resize", function () {
   canvas.height = window.innerHeight;
